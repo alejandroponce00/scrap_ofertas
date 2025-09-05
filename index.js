5a29495c5972be857cad32e44b19d1f278fd5533
alejandro <ts.com>
2025-09-05 20:09:29 -0300

    agregando github actions

--- index.js
+++ index.js
@@ -15,15 +15,16 @@ async function scrapearProductos() {
   const browser = await launchBrowser();
   const page = await browser.newPage();
 
-  await page.goto(
-    'https://www.cotodigital.com.ar/sitios/cdigi/categoria/ofertas-exclusivas/_/N-1nx2iz5%3FNf%3Dproduct.startDate%257CLTEQ%2B1.755648E12%257C%257Cproduct.endDate%257CGTEQ%2B1.755648E12&Nr%3DAND%2528product.sDisp_200%253A1004%252Cproduct.language%253Aespa%25C3%25B1ol%252COR%2528product.siteId%253ACotoDigital%2529%2529&pushSite%3DCotoDigital',
-    { waitUntil: 'networkidle2' }
-  );
+await page.goto(
+  'https://www.cotodigital.com.ar/sitios/cdigi/categoria/ofertas-exclusivas/_/N-1nx2iz5%3FNf%3Dproduct.startDate%257CLTEQ%2B1.755648E12%257C%257Cproduct.endDate%257CGTEQ%2B1.755648E12&Nr%3DAND%2528product.sDisp_200%253A1004%252Cproduct.language%253Aespa%25C3%25B1ol%252COR%2528product.siteId%253ACotoDigital%2529%2529&pushSite%3DCotoDigital',
+  { waitUntil: 'networkidle0' } // espera a que no haya ninguna solicitud pendiente
+);
+
 
   await page.waitForSelector('.nombre-producto.cursor-pointer', { timeout: 150000 });
 
   const productos = await page.evaluate(() => {
-    const nombres = document.querySelectorAll('nombre-producto cursor-pointer');
+    const nombres = document.querySelectorAll('.nombre-producto.cursor-pointer');
     const precios = document.querySelectorAll('.card-title.text-center.mt-1.m-0.p-0.ng-star-inserted');
     const resultados = [];
     for (let i = 0; i < nombres.length; i++) {
